28a1f26f95a6c0baabcd09602423e3357e2ce266
Stefan Klumpp <stefan.klumpp@gmail.com>
2012-04-06 18:47:22 +0200

    9.3 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update] # redirect if not signed-in
+  before_filter :signed_in_user, only: [:index, :edit, :update] # redirect if not signed-in
   before_filter :correct_user,   only: [:edit, :update] # redirect to root_path
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+
 
   def show
     @user = User.find(params[:id])
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,6 +27,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
 
+      it { should have_link('Users',    href: users_path) }
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
@@ -48,6 +49,7 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    # Non-signed-in users:
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -62,6 +64,12 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
       end
 
       describe "when attempting to visit a protected page" do
@@ -82,6 +90,8 @@ describe "Authentication" do
 
     end
 
+
+    # Wrong user (other user):
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
