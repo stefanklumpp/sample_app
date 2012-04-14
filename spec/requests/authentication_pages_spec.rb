84a89191cc0ecc530187a3ce733c1c381ede67a1
Stefan Klumpp <stefan.klumpp@gmail.com>
2012-04-14 18:54:21 +0200

    11.2.4 complete

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -80,6 +80,18 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
 
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { response.should redirect_to(signin_path) }
+          end
+        end
+
       end
 
       describe "when attempting to visit a protected page" do
