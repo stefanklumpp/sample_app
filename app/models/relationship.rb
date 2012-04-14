7d7b481c39c2a0002359479553978a9e6c6c2a41
Stefan Klumpp <stefan.klumpp@gmail.com>
2012-04-14 15:34:07 +0200

    11.1.2 complete

---
+++ app/models/relationship.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer         not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
+class Relationship < ActiveRecord::Base
+  attr_accessible :followed_id
+
+  belongs_to :follower, class_name: "User"  # user_id
+  belongs_to :followed, class_name: "User"  # user_id
+
+end
