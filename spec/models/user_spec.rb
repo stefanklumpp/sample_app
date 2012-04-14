ebb7d4df36fdc1104973411d1dd8a6e41f26fb17
Stefan Klumpp <stefan.klumpp@gmail.com>
2012-04-14 17:19:29 +0200

    11.1.5 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,8 @@ describe User do
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
   it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
@@ -192,6 +194,14 @@ describe User do
 
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
     end
 
   end
